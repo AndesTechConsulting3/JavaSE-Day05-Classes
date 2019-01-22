fb888f6d5aadae015dc994166bef1c4f8c98ff12
a0z9 <a0z9@rambler.ru>
2019-01-22 17:24:48 +0300

    1st commit

--- src/main/java/org/andestech/learning/rfb19/g3/AppObject.java
+++ src/main/java/org/andestech/learning/rfb19/g3/AppObject.java
@@ -1,9 +1,13 @@
 package org.andestech.learning.rfb19.g3;
 
+class B2{ int data;}
+
 class A2 implements Cloneable
 {
     int data;
     public A2(int data){this.data = data;}
+
+    B2 b2;
 
     @Override
     protected Object clone() throws CloneNotSupportedException {
@@ -19,12 +23,18 @@ public class AppObject {
     public static void main(String[] args) throws CloneNotSupportedException
     {
         A2 a = new A2(22);
+        a.b2 = new B2();
+        a.b2.data = 87687;
+
         A2 a2 = (A2) a.clone();
 
         System.out.println(a);
         System.out.println(a2);
 
+        System.out.println("---------------------");
 
+        System.out.println("a data=" + a.data + ",a b2 data=" + a.b2.data);
+        System.out.println("a2 data=" + a2.data + ",a2 b2 data=" + a2.b2.data);
 
     }
 }
